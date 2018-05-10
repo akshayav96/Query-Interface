21dd70530637cf0f7ac8042dc01d8fc1613a35f3
Keane Gonzalez <keane.gonzalez@gmail.com>
2018-05-10 13:02:17 -0700

    Dummy file for placeholder data

---
+++ vs_pmid.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 12:35:46 2018
+
+@author: Keane
+"""
+#placeholder for Vector Space Model results
+
+
+
+def vs_pmid():
+    
+    pmid_output =[]
+    for ii in range(0,20):
+        pmid_output.append(ii)
+    
+    return pmid_output
+    
