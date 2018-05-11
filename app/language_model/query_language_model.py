7dc2aedd753b95f3db2ae15e78b2e57538d3b33a
jspolson <j.s.polson@gmail.com>
2018-05-10 23:28:23 -0700

    updating empty query

--- app/language_model/query_language_model.py
+++ app/language_model/query_language_model.py
@@ -23,19 +23,21 @@ def query_language_model(query):
     q_terms = query.translate(str.maketrans(" ", " ", string.punctuation+'0123456789'))
     q_terms = [SnowballStemmer("english").stem(w) for w in word_tokenize(q_terms) if w in lm_dict['terms']]
     q_terms = [w for w in q_terms if not w in set(stopwords.words('english'))]
-    query_vector = np.asarray(pd.Series(pd.Series(q_terms).value_counts(normalize = True), index = lm_dict['terms']).fillna(0.00001))
 
-    #create vector only of term length
-    subset = pdf_vectors[:,[lm_dict['terms'].index(i) for i in q_terms]]
-    #subset - this keeps all of the records, with the original shape of the language model
-    relevant = pdf_vectors[~(subset==0).all(1)]
-    #subset - this keeps all of the records with the shape of the query language model
-    #relevant = subset[~(subset==0).all(1)]
+    if len(q_terms) == 0:
+        print( "There is not a single document that matches the terms in this query.")
+    else:
+        query_vector = np.asarray(pd.Series(pd.Series(q_terms).value_counts(normalize = True), index = lm_dict['terms']).fillna(0.00001))
 
-    contained_pmids = lm_dict['PMID'][np.nonzero(np.sum(subset, axis = 1))[0]]
+        #create vector only of term length
+        subset = pdf_vectors[:,[lm_dict['terms'].index(i) for i in q_terms]]
+        #subset - this keeps all of the records, with the original shape of the language model
+        relevant = pdf_vectors[~(subset==0).all(1)]
+        #subset - this keeps all of the records with the shape of the query language model
+        #relevant = subset[~(subset==0).all(1)]
 
-    scores = {}
-    for i, j in zip(np.arange(0, len(relevant),1), contained_pmids):
-        scores[j] = entropy(relevant[i], query_vector)
-
-    return sorted(scores, key=scores.get)[:20]
+        contained_pmids = lm_dict['PMID'][np.nonzero(np.sum(subset, axis = 1))[0]]
+        scores = {}
+        for i, j in zip(np.arange(0, len(relevant),1), contained_pmids):
+            scores[j] = entropy(relevant[i], query_vector)
+        return sorted(scores, key=scores.get)[:20]
